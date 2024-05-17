6c7ba335e3ffca98bdf8a63dcb4ce6ff5a60e92a
Logan Jones <lpj11101@gmail.com>
2024-05-17 12:08:55 -0700

    Added previous Data folder

--- FakeArchive/Data/MainFakeArchive.cs
+++ FakeArchive/Data/MainFakeArchive.cs
@@ -1,6 +1,18 @@
-﻿namespace FakeArchive.Data
+﻿using FakeArchive.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace FaveArchive.Data
 {
-    public class MainFakeArchive
+    public class MainFakeArchiveContext : DbContext
     {
-    }
-}
+        public MainFakeArchiveContext(DbContextOptions<MainFakeArchiveContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Show> Shows { get; set; }
+        public DbSet<Studio> Studios { get; set; }
