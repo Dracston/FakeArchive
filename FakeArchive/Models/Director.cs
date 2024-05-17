72b3636716d964de4df3248f0795b744855e6f3f
Logan Jones <lpj11101@gmail.com>
2024-05-17 12:09:45 -0700

    updated Director Model

--- FakeArchive/Models/Director.cs
+++ FakeArchive/Models/Director.cs
@@ -1,6 +1,22 @@
-﻿namespace FakeArchive.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeArchive.Models
 {
     public class Director
     {
+
+        public int directorId { get; set; }
+
+
+
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Range(0, 120)]
+        public int Age { get; set; }
+
+
     }
 }
